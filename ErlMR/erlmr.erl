68cebfed342bd5b1b4ca19b7d5171b761351df47
sKabYY <skabyy@gmail.com>
2013-11-26 17:12:58 +0800

    .

--- ErlMR/erlmr.erl
+++ ErlMR/erlmr.erl
@@ -1,13 +1,20 @@
 -module(erlmr).
--export([mrgo/5,
+-export([mapreduce/5,
          mk_input_format_from_list/1,
+         default_map/2,
+         default_reduce/1,
+         default_output_format/1,
          pprint/1]).
-
-pprint(Datum) -> io:format("~p~n", [Datum]).
 
 % map: {k1, v1} -> [{k2, v2}]
 % reduce: {k2, [v2]} -> {k2, v3}
 
+% some usefull functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% pretty print
+pprint(Datum) -> io:format("~p~n", [Datum]).
+
+% make input format from a list
 mk_input_format_from_list(L) ->
     fun () ->
         case L of
@@ -16,45 +23,77 @@ mk_input_format_from_list(L) ->
         end
     end.
 
-% N: the max number of tasks
+% default map
+default_map({K, V}, Emit) -> Emit({K, V}).
+
+% default reduce
+default_reduce({_K, ListOfV}) -> ListOfV.
+
+% default output_format
+default_output_format({K, V}) ->
+    pprint({K, V}),
+    fun default_output_format/1.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% N: the max number of workers
 % Map: {K, V} X EmitIntermediate -> void
 % EmitIntermediate: {K, V} -> void
 % Reduce: {K, [V]} -> V
 % InputFormat () -> {{K, V}, NextInputFormat} | eof
 % OutputFormat {K, V} -> NextOutputFormat
 
-mrgo(N, InputFormat, OutputFormat, Map, Reduce) ->
-    InputFormatProc = spawn(fun () -> input_format_proc(InputFormat) end),
-    EmitIntermediateProc = spawn(fun () -> emit_intermediate_proc() end),
+mapreduce(N, InputFormat, OutputFormat, Map, Reduce) ->
+    process_flag(trap_exit, true),
+    InputFormatProc = start_tracker(
+                        fun () ->
+                                input_format_proc(InputFormat)
+                        end),
+    EmitIntermediateProc = start_tracker(
+                             fun () ->
+                                     emit_intermediate_proc()
+                             end),
     EmitIntermediate = fun (KV) -> EmitIntermediateProc ! {emit, KV} end,
-    MapProcs = start_map_procs(N, self(), InputFormatProc, Map, EmitIntermediate),
-    wait(MapProcs),
-    stop(InputFormatProc),
-    IntermediateData = pull_and_stop(EmitIntermediateProc),
-    IntermediateInputProc =
-        spawn(fun () ->
-                      IntermediateInput = mk_input_format_from_list(IntermediateData),
-                      input_format_proc(IntermediateInput)
-              end),
-    OutputFormatProc = spawn(fun () -> output_format_proc(OutputFormat) end),
-    ReduceProcs = start_reduce_procs(N, self(), IntermediateInputProc, OutputFormatProc, Reduce),
-    wait(ReduceProcs),
-    stop(IntermediateInputProc),
-    stop(OutputFormatProc).
+    MapProcs = start_workern(
+                 N,
+                 fun () ->
+                         map_proc(Map,
+                                  InputFormatProc,
+                                  EmitIntermediate)
+                 end),
+    wait_workers(MapProcs),
+    stop_tracker(InputFormatProc),
+    IntermediateData = pull_and_stop_tracker(EmitIntermediateProc),
+    IntermediateInput = mk_input_format_from_list(IntermediateData),
+    IntermediateInputProc = start_tracker(
+                              fun () ->
+                                      input_format_proc(IntermediateInput)
+                              end),
+    OutputFormatProc = start_tracker(
+                         fun () ->
+                                 output_format_proc(OutputFormat)
+                         end),
+    ReduceProcs = start_workern(
+                    N,
+                    fun() ->
+                            reduce_proc(Reduce,
+                                        IntermediateInputProc,
+                                        OutputFormatProc)
+                    end),
+    wait_workers(ReduceProcs),
+    stop_tracker(IntermediateInputProc),
+    stop_tracker(OutputFormatProc).
 
 % two types of processes: worker and tracker
-% worker: map_proc and reduce_proc
-% tracker: ...
+% worker: map_proc and reduce_proc, stop after receive 'eof'
+% tracker: stop after receive 'stop'
 
-wait([]) -> done;  % TODO
-wait(Pids) ->
-    receive
-        Pid -> wait([X || X <- Pids, X =/= Pid])
-    end.
+% tracker
+start_tracker(F) -> spawn(F).
 
-stop(Pid) -> Pid ! stop, ok.
+stop_tracker(Pid) -> Pid ! stop, ok.
 
-pull_and_stop(Pid) ->
+pull_and_stop_tracker(Pid) ->
     Pid ! {pull, self()},
     receive
         Datum ->
@@ -62,6 +101,23 @@ pull_and_stop(Pid) ->
             Datum
     end.
 
+% worker
+start_workern(N, F) ->
+    start_workern_iter([], N, F).
+
+start_workern_iter(Acc, 0, _) -> Acc;
+start_workern_iter(Acc, N, F) ->
+    Pid = spawn_link(F),
+    start_workern_iter([Pid|Acc], N - 1, F).
+
+wait_workers([]) -> ok;
+wait_workers(Pids) ->
+    receive
+        {'EXIT', Pid, _Why} ->
+            wait_workers([X || X <- Pids, X =/= Pid])
+    end.
+
+% emit proc
 emit_intermediate_proc() ->
     emit_intermediate_proc_iter(dict:new()).
 
@@ -72,9 +128,10 @@ emit_intermediate_proc_iter(Bucket) ->
         {pull, From} ->
             From ! dict:to_list(Bucket),
             emit_intermediate_proc_iter(Bucket);
-        stop -> done
+        stop -> ok
     end.
 
+% input format proc
 input_format_proc(InputFormat) ->
     receive
         {From, next} ->
@@ -86,43 +143,34 @@ input_format_proc(InputFormat) ->
                     From ! eof,
                     input_format_proc(InputFormat)
             end;
-        stop -> done
+        stop -> ok
     end.
 
+% output format proc
 output_format_proc(OutputFormat) ->
     receive
         {output, KV} ->
             output_format_proc(OutputFormat(KV));
-        stop -> done
+        stop -> ok
     end.
 
-start_map_procs(0, _, _, _, _) -> [];
-start_map_procs(N, MainPid, InputFormatProc, Map, EmitIntermediate) when N > 0 ->
-    Pid = spawn(fun () -> map_proc(MainPid, InputFormatProc, Map, EmitIntermediate) end),
-    [Pid|start_map_procs(N - 1, MainPid, InputFormatProc, Map, EmitIntermediate)].
-
-map_proc(MainPid, InputFormatProc, Map, EmitIntermediate) ->
+% map proc
+map_proc(Map, InputFormatProc, EmitIntermediate) ->
     InputFormatProc ! {self(), next},
     receive
         {K, V} ->
             Map({K, V}, EmitIntermediate),
-            map_proc(MainPid, InputFormatProc, Map, EmitIntermediate);
-        eof ->
-            MainPid ! self()
+            map_proc(Map, InputFormatProc, EmitIntermediate);
+        eof -> ok
     end.
 
-start_reduce_procs(0, _, _, _, _) -> [];
-start_reduce_procs(N, MainPid, IntermediateInputProc, OutputFormatProc, Reduce) ->
-    Pid = spawn(fun () -> reduce_proc(MainPid, IntermediateInputProc, OutputFormatProc, Reduce) end),
-    [Pid|start_reduce_procs(N - 1, MainPid, IntermediateInputProc, OutputFormatProc, Reduce)].
-
-reduce_proc(MainPid, IntermediateInputProc, OutputFormatProc, Reduce) ->
+% reduce proc
+reduce_proc(Reduce, IntermediateInputProc, OutputFormatProc) ->
     IntermediateInputProc ! {self(), next},
     receive
         {K, ListOfV} ->
             V = Reduce({K, ListOfV}),
             OutputFormatProc ! {output, {K, V}},
-            reduce_proc(MainPid, IntermediateInputProc, OutputFormatProc, Reduce);
-        eof ->
-            MainPid ! self()
+            reduce_proc(Reduce, IntermediateInputProc, OutputFormatProc);
+        eof -> ok
     end.
--- ErlMR/test.erl
+++ ErlMR/test.erl
@@ -1,14 +1,9 @@
 -module(test).
 -export([go/0]).
--import(erlmr, [mrgo/5, mk_input_format_from_list/1]).
 
 map({K, V}, Emit) ->
     Emit({K, V}),
     Emit({K, V * V}).
-
-output_format({K, V}) ->
-    io:format("~p => ~p~n", [K, V]),
-    fun output_format/1.
 
 reduce({_, ListOfV}) when is_list(ListOfV) ->
     lists:sum(ListOfV).
@@ -17,9 +12,9 @@ go() ->
     TestInput = [{a, 1},
                  {b, 2},
                  {c, 3}],
-    InputFormat = mk_input_format_from_list(TestInput),
-    mrgo(4,
-         InputFormat,
-         fun output_format/1,
-         fun map/2,
-         fun reduce/1).
+    erlmr:mapreduce(
+      4,
+      erlmr:mk_input_format_from_list(TestInput),
+      fun erlmr:default_output_format/1,
+      fun map/2,
+      fun reduce/1).
