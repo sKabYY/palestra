8ae0e025bdee34d67918e88a6146c0eb6f7ee579
sKabYY <skabyy@gmail.com>
2014-01-30 11:47:59 +0800

    .

---
+++ pllc/v2/iswim.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+(define (interp e) (value-of e))
+
+(define (value-of exp1)
+  (match exp1
+    ; a variable
+    [(? symbol? s) s]
+    ; a basic constant
+    [(? number? x) x]
+    ; a procedure
+    [`(lambda (,a) ,b) (make-procedure a b)]
+    ; primitive operations
+    [`(add1 ,e) (+ (value-of e) 1)]
+    [`(sub1 ,e) (- (value-of e) 1)]
+    [`(iszero ,e) (= (value-of e) 0)]
+    [`(+ ,e1 ,e2) (+ (value-of e1) (value-of e2))]
+    [`(- ,e1 ,e2) (- (value-of e1) (value-of e2))]
+    [`(* ,e1 ,e2) (+ (value-of e1) (value-of e2))]
+    ; an application
+    [`(,e1 ,e2) (proc-apply (value-of e1) (value-of e2))]))
+
+(define (substitute var val exp1)
+  (define (>> e) (substitute var val e))
+  (match exp1
+    ; a variable
+    [(? symbol? s)
+     (if (eqv? s var) val s)]
+    ; a basic constant
+    [(? number? x) x]
+    ; a procedure
+    [`(lambda (,a) ,b)
+     (if (eqv? a var)
+         exp1
+         `(lambda (,a) ,(>> b)))]
+    ; primitive operations
+    [`(add1 ,e) `(add1 ,(>> e))]
+    [`(sub1 ,e) `(sub1 ,(>> e))]
+    [`(iszero ,e) `(iszero ,(>> e))]
+    [`(+ ,e1 ,e2) `(+ ,(>> e1) ,(>> e2))]
+    [`(- ,e1 ,e2) `(- ,(>> e1) ,(>> e2))]
+    [`(* ,e1 ,e2) `(+ ,(>> e1) ,(>> e2))]
+    ; an application
+    [`(,e1 ,e2) `(,(>> e1) ,(>> e2))]))
+
+(define (check prad v msg)
+  (if (prad v)
+      (void)
+      (error msg "got:" v)))
+
+; procedure ;;;
+
+(struct procedure (var body))
+
+(define (make-procedure var body)
+  (check symbol? var "need symbol")
+  (procedure var body))
+
+(define (proc-apply proc val)
+  (value-of (substitute (procedure-var proc) val (procedure-body proc))))
+
+(require "driver-loop.rkt")
+(driver-loop interp)
